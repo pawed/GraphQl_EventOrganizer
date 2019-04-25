78b13f524a1cd8353e022cbeb95541de0d1ed540
paweld <pawel.dllugosz@gmail.com>
2019-04-25 19:31:33 +0200

    Add Mutation

---
+++ GraphQLApi/GraphQLElments/Mutation/PeopleMutation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using GraphQLApi.Interfaces;
+
+namespace GraphQLApi.GraphQLElments.Mutation
+{
+    public class PeopleMutation:ObjectGraphType
+    {
+
+        public PeopleMutation(IPeopleRepository repository)
+        {
+            Field<PeopleGraphType>
+        }
+
+    }
+}
